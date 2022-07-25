89dbf753aa60a58b32ab6ef3865c0274bfdf5a8b
nickadamson <naburner@protonmail.com>
2022-07-25 15:32:35 -0700

    feat: 🎸 add source serif 4 font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "@fontsource/source-serif-4/400.css";
+import "@fontsource/source-serif-4/variable-full.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

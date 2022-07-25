3c52b837111a76ef60712b9f0587c18769d33a7c
nickadamson <naburner@protonmail.com>
2022-07-25 15:39:34 -0700

    feat: create project-card component

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { Dispatch, FC, MouseEventHandler, SetStateAction } from "react";
+import { Project } from "../pages/projects";
+
+interface ProjectCardProps {
+  project: Project;
+  setActive: Dispatch<SetStateAction<string>>;
+}
+
+const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
+  const { name, github, link, stack, description } = project;
+
+  return (
+    <div className="flex flex-col p-2 w-full h-24 border-2 max-w-1/3">
+      <div className="flex justify-between h-full">
+        <div className="flex flex-col justify-between h-full text-left">
+          <h3 className="text-xl font-semibold">
+            <div className="flex space-x-1">
+              <a href={link}>&#128279; {name}</a>
+              <a className="self-end" href={github}>
+                <div className="text-base font-normal">
+                  {" - "}
+                  <Image
+                    src="/github.svg"
+                    alt="Github Repo"
+                    width={16}
+                    height={16}
+                  />{" "}
+                  Source
+                </div>
+              </a>
+            </div>
+          </h3>
+          <p className="text-sm italic font-light tracking-wide">{stack}</p>
+        </div>
+        <div className="w-1/2">
+          <p className="text-right">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,72 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Layout from "../components/Layout";
+import ProjectCard from "../components/ProjectCard";
 
-const Projects: NextPage = () => {
-  return <div>Projects</div>;
+export interface Project {
+  name: string;
+  github: string;
+  link: string;
+  stack: string;
+  description: string;
+}
+
+const projects: Project[] = [
+  {
+    name: "Loudverse",
+    github: "https://github.com/neodaoist/loudverse/",
+    link: "https://loudverse.com/",
+    stack: "Solidity, NextJS",
+    description: "2 sided marketplace for public goods",
+  },
+  {
+    name: "ourz.network",
+    github: "https://github.com/ourz-network/",
+    link: "https://ourz.network/",
+    stack: "Solidity, NextJS",
+    description: "split royalties for collaborators",
+  },
+  {
+    name: "messonry",
+    github: "https://github.com/nickadamson/messonry/",
+    link: "https://nickadamson.github.io/messonry/?path=/story/example-introduction--page",
+    stack: "React",
+    description: "npm library for react to create a messy grid",
+  },
+];
+
+const ProjectsPage: NextPage = () => {
+  const router = useRouter();
+  const { query } = router;
+  console.log(query);
+
+  const [active, setActive] = useState((query[`""`] as string) ?? "");
+
+  return (
+    <Layout>
+      <section id="about">
+        <div className="flex flex-col">
+          <h2 className="text-xl font-semibold">Projects</h2>
+          <div className="flex flex-col justify-evenly space-y-4 w-full">
+            {active === "" ? (
+              projects.map((project) => {
+                return (
+                  <ProjectCard
+                    key={project.name}
+                    project={project}
+                    setActive={setActive}
+                  />
+                );
+              })
+            ) : (
+              <>{/* <FullProject /> */}</>
+            )}
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
-export default Projects;
+export default ProjectsPage;

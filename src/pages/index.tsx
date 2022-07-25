b46964be59ab8be35179a28c2bf95ba34da37874
nickadamson <naburner@protonmail.com>
2022-07-25 16:28:57 -0700

    feat: add aboutme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,30 @@
 import type { NextPage } from "next";
-import Head from "next/head";
+import Layout from "../components/Layout";
 
 const AboutPage: NextPage = () => {
   return (
-    <div className="">
-      <Head>
-        <title></title>
-        <meta name="" content="" />
-        <link rel="" href="" />
-      </Head>
-
-      <main className=""></main>
+    <Layout>
+      <section id="about">
+        <div className="flex flex-col">
+          <h2 className="mb-2 text-xl font-semibold">About</h2>
+          <div className="space-y-2 text-lg">
+            <p>
+              Nick is a developer who particularly <i>loves</i> open-source and
+              decentralized technology.
+            </p>
+            <p>
+              Before discovering this interest in programming, it was instead in
+              photo + video production -- working as a digital tech on fashion
+              shoots.
+            </p>
+            <p>
+              That creative experience serves as a source of inspiration for the
+              projects being built now.
+            </p>
     </div>
+        </div>
+      </section>
+    </Layout>
   );
 };
 

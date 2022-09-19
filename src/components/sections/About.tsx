6be0daeffdaca8cb3fb5748050688851f417ae33
nickadamson <naburner@protonmail.com>
2022-09-19 12:24:29 -0700

    fix: 🐛 center div when vertical

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -29,58 +29,58 @@ const About = () => {
   }, [control, inView]);
 
   return (
-    <section id="about">
-      <div
-        ref={ref}
-        className="flex flex-col px-2 py-24 w-full lg:flex-row h-center-action"
+    <section
+      ref={ref}
+      id="about"
+      className="flex flex-col px-2 w-full lg:flex-row h-center-action portrait:my-16"
+    >
+      <m.div
+        variants={headerVariants}
+        initial="hidden"
+        animate={control}
+        className="flex justify-center h-full lg:justify-end lg:w-1/3"
       >
-        <m.div
-          variants={headerVariants}
-          initial="hidden"
-          animate={control}
-          className="flex justify-center lg:justify-end lg:w-1/3"
-        >
-          <h2 className="self-end my-auto text-2xl font-black text-center lg:rotate-180 lg:text-8xl lg:section-header">
-            About
-          </h2>
-        </m.div>
-        <m.div
-          variants={bodyVariants}
-          initial="hidden"
-          animate={control}
-          className="p-1 ml-0 space-y-6 lg:my-auto lg:w-3/5 md:p-8 lg:text-xl"
-        >
-          <p>
-            I&apos;m a Full-Stack Engineer with a passion for open-source
-            software. I love creating striking experiences, building useful
-            tools, and bringing ideas to reality.
-          </p>
-          <p>
-            Previously, I was a freelance Digital Imaging Technician for over 5
-            years. I assisted fashion and make-up brands in producing beautiful
-            content. The resourcefulness and eye for design I developed is
-            especially useful in my programming work now.
-          </p>
-          <p>
-            Check out all my code{" "}
-            <a
-              href={"https://github.com/nickadamson"}
-              rel="noopener noreferrer"
-              target="_blank"
-              className="underline"
-            >
-              on github
-            </a>
-            , add me{" "}
-            <a
-              href={"https://www.linkedin.com/in/nick-adamson/"}
-              rel="noopener noreferrer"
-              target="_blank"
-              className="underline"
-            >
-              on linkedin
-            </a>
-            {/* , follow me{" "}
+        <h2 className="justify-self-center self-end my-auto h-full text-2xl font-black text-center lg:rotate-180 lg:text-8xl lg:section-header">
+          About
+        </h2>
+      </m.div>
+      <m.div
+        variants={bodyVariants}
+        initial="hidden"
+        animate={control}
+        className="p-1 ml-0 space-y-6 lg:my-auto lg:w-3/5 md:p-8 lg:text-xl"
+      >
+        <p>
+          I&apos;m a Full-Stack Engineer with a passion for open-source
+          software. I love creating striking experiences, building useful tools,
+          and bringing ideas to reality.
+        </p>
+        <p>
+          Previously, I was a freelance Digital Imaging Technician for over 5
+          years. I assisted fashion and make-up brands in producing beautiful
+          content. The resourcefulness and eye for design I developed is
+          especially useful in my programming work now.
+        </p>
+        <p>
+          Check out all my code{" "}
+          <a
+            href={"https://github.com/nickadamson"}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="underline"
+          >
+            on github
+          </a>
+          , add me{" "}
+          <a
+            href={"https://www.linkedin.com/in/nick-adamson/"}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="underline"
+          >
+            on linkedin
+          </a>
+          {/* , follow me{" "}
             <a
               href={"https://twitter.com/nickadams0n"}
               rel="noopener noreferrer"
@@ -89,18 +89,17 @@ const About = () => {
             >
               on twitter
             </a> */}
-            , or reach out{" "}
-            <a
-              href={"mailto:nickadamson@pm.me"}
-              rel="noopener noreferrer"
-              className="underline"
-            >
-              via email
-            </a>
-            .
-          </p>
-        </m.div>
-      </div>
+          , or reach out{" "}
+          <a
+            href={"mailto:nickadamson@pm.me"}
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            via email
+          </a>
+          .
+        </p>
+      </m.div>
     </section>
   );
 };

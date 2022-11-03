8cd19953d145e794ea9e33e761821a3f85e18b09
nickadamson <naburner@protonmail.com>
2022-11-03 16:16:05 -0700

    feat: 🎸 add vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import "../styles/globals.css";
 import "@fontsource/ibm-plex-mono";
 import "@fontsource/source-serif-4/400.css";
 import "@fontsource/source-serif-4/variable-full.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;

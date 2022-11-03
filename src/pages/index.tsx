553d41b09e6a09ca30564d79a9a24c98b09e4f2a
nickadamson <naburner@protonmail.com>
2022-11-03 15:54:08 -0700

    perf: ⚡️ use dynamic imports + suspense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,36 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
-import About from "../components/sections/About";
-import Landing from "../components/sections/Landing";
-import Projects from "../components/sections/Projects";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+import { LazyMotion, domAnimation } from "framer-motion";
+
+const DynamicAbout = dynamic(() => import("../components/sections/About"), {
+  suspense: true,
+});
+const DynamicLanding = dynamic(() => import("../components/sections/Landing"), {
+  suspense: true,
+});
+
+const DynamicProjects = dynamic(
+  () => import("../components/sections/Projects"),
+  {
+    suspense: true,
+  }
+);
 
 const LandingPage: NextPage = () => {
   return (
-    <Layout>
-      <main className="flex overflow-x-hidden overflow-y-scroll fixed top-0 right-0 bottom-0 left-0 z-10 flex-col mx-auto mb-44 max-h-full font-serif border border-black my-h-safe-action scrollbar-hidden w-center-title h-center-action scroll-smooth">
-        <Landing />
-        <Projects />
-        <About />
-      </main>
-    </Layout>
+    <LazyMotion strict features={domAnimation}>
+      <Layout>
+        <main className="flex overflow-x-hidden overflow-y-scroll fixed top-0 right-0 bottom-0 left-0 z-10 flex-col mx-auto mb-44 max-h-full font-serif border border-black my-h-safe-action scrollbar-hidden w-center-title h-center-action scroll-smooth">
+          <Suspense fallback={"hi :)"}>
+            <DynamicLanding />
+            <DynamicProjects />
+            <DynamicAbout />
+          </Suspense>
+        </main>
+      </Layout>
+    </LazyMotion>
   );
 };
 
